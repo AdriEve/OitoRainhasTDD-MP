44556af5b3fc0baf80d28fb1ac3eb1de77472d19
AdriEve <108773502+AdriEve@users.noreply.github.com>
2023-06-13 21:05:58 -0300

    Teste 1 - Tabuleiro vazio

--- rainhas.hpp
+++ rainhas.hpp
@@ -1,1 +1,5 @@
+// Copyright [2023] <Adriele>
+#ifndef RAINHAS_HPP_
+#define RAINHAS_HPP_
 
+#endif  // RAINHAS_HPP_
---
+++ testa_rainhas.cpp
@@ -0,0 +1,9 @@
+// Copyright [2023] <Adriele>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "rainhas.hpp"
+
+TEST_CASE("Teste 1 -tabuleiro vazio") {
+    Rainhas t1({});
+    assert(t1.solucao() == -1);
+}
