5299424c6cfe67942b89d6bb1f151d73f8220e6a
AdriEve <108773502+AdriEve@users.noreply.github.com>
2023-06-13 21:11:52 -0300

    Teste 2 - Sem damas

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -7,3 +7,9 @@ TEST_CASE("Teste 1 -tabuleiro vazio") {
     Rainhas t1({});
     assert(t1.solucao() == -1);
 }
+
+TEST_CASE("Teste 2 -sem damas") {
+    std::vector<std::string> board = {"00000000", "00000000","00000000","00000000","00000000","00000000","00000000","00000000"};
+    Queens t2(board);
+    assert(t2.solve() == -1);
+}
